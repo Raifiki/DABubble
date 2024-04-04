0df85bfb8576b68dec1e7bb8ac7af0843c55ab70
ChristianWestrich <christian.westrich@gmx.de>
2024-04-04 12:06:07 +0200

    finish static login & PrivacyPolicy & imprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
- { path: '', component: AuthenticationComponent }
+ { path: '', component: AuthenticationComponent },
+ { path: 'imprint', component: ImprintComponent },
+ { path: 'privacy', component: PrivacyPolicyComponent }
 
 ];
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  lgnDisabled:boolean = true;
 
+  onSubmit() {
+   
+  }
 }
