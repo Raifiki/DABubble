6b7853ccf462daa2c8b90eaf3a55d7efede23844
Raifikie <leonard_weiss@web.dem>
2024-04-04 13:32:49 +0200

    component sendemail: add component and add route /sendemail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,14 @@ import { AuthenticationComponent } from './authentication/authentication.compone
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { ChooseavatarComponent } from './chooseavatar/chooseavatar.component';
+import { SendemailComponent } from './sendemail/sendemail.component';
 
 export const routes: Routes = [
  { path: '', component: AuthenticationComponent },
  { path: 'imprint', component: ImprintComponent },
  { path: 'privacy', component: PrivacyPolicyComponent },
- {path: 'chooseAvatar', component: ChooseavatarComponent} // nur temprär, diskussion in Team wie wir das ganze einbinden
+ { path: 'chooseAvatar', component: ChooseavatarComponent }, // nur temprär, diskussion in Team wie wir das ganze einbinden
+ { path: 'sendemail', component: SendemailComponent } // nur temprär, diskussion in Team wie wir das ganze einbinden
+ 
 
 ];
