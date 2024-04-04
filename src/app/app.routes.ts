d6ee74ae992859c49620265aa13291c23ede00f4
ChristianWestrich <christian.westrich@gmx.de>
2024-04-04 15:48:43 +0200

    create signIn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,12 @@ import { AuthenticationComponent } from './authentication/authentication.compone
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { ChooseavatarComponent } from './chooseavatar/chooseavatar.component';
+import { SigninComponent } from './authentication/signin/signin.component';
 
 export const routes: Routes = [
  { path: '', component: AuthenticationComponent },
  { path: 'imprint', component: ImprintComponent },
  { path: 'privacy', component: PrivacyPolicyComponent },
- {path: 'chooseAvatar', component: ChooseavatarComponent} // nur temprär, diskussion in Team wie wir das ganze einbinden
-
+ { path: 'chooseAvatar', component: ChooseavatarComponent }, // nur temprär, diskussion in Team wie wir das ganze einbinden
+ { path: 'signin', component: SigninComponent }
 ];
--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
+checkboxChecked = false
+
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
+
+
+  goBack() {
+    this.router.navigate(['..'] ,{relativeTo: this.route})
+  }
+
+  onSubmit(form: NgForm){
+
+  }
+
+  checkboxChange() {
+   this.checkboxChecked = !this.checkboxChecked
+
+  }
+
 
 }
