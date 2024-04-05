3c9104403e81d5302740f99ec5ecdc14c3a25f59
Raifikie <leonard_weiss@web.dem>
2024-04-05 09:40:56 +0200

    component resetpassword: add overly anmelden

--- src/app/resetpassword/resetpassword.component.ts
+++ src/app/resetpassword/resetpassword.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+
+// import custom components
+import { OverlayanmeldenComponent } from './overlayanmelden/overlayanmelden.component';
 
 @Component({
   selector: 'app-resetpassword',
   standalone: true,
-  imports: [ FormsModule ],
+  imports: [ FormsModule,
+  OverlayanmeldenComponent ],
   templateUrl: './resetpassword.component.html',
   styleUrl: './resetpassword.component.scss'
 })
 export class ResetpasswordComponent {
   newPassword!:string;
   confirmPassword!:string;
+
+  toggleOverlay:boolean = true;
+
+  onSubmit(form:NgForm){
+    if(form.valid){
+      this.toggleOverlay = !this.toggleOverlay;
+      form.reset();
+    }
+  }
 }
--- src/app/sendemail/sendemail.component.ts
+++ src/app/sendemail/sendemail.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { OverlayemailsendComponent } from './overlayemailsend/overlayemailsend.component';
 
 @Component({
   selector: 'app-sendemail',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,
+  OverlayemailsendComponent],
   templateUrl: './sendemail.component.html',
   styleUrl: './sendemail.component.scss'
 })
 export class SendemailComponent {
   eMail!:string;
   test!:string;
+
+  toggleOverlay:boolean = true;
+
+  onSubmit(form:NgForm){
+    if(form.valid){
+      this.toggleOverlay = !this.toggleOverlay;
+      form.reset();
+    }
+  }
+
 }
