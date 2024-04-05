b8cc0cc9c08e811949bff14041e6c35092959b37
Raifikie <leonard_weiss@web.dem>
2024-04-05 07:41:42 +0200

    component resetPassword: add HTML and CSS first version, add PW validation

--- src/app/resetpassword/resetpassword.component.ts
+++ src/app/resetpassword/resetpassword.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-resetpassword',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './resetpassword.component.html',
   styleUrl: './resetpassword.component.scss'
 })
 export class ResetpasswordComponent {
-
+  newPassword!:string;
+  confirmPassword!:string;
 }
