6d04d337cc8bdc89e5656b84c06c358dbd8d4061
Raifikie <leonard_weiss@web.dem>
2024-04-07 20:04:16 +0200

    component userlistitem: add component userlistitem and first version of HTML and CSS + functionality

--- src/app/createchannel/createchannel.component.ts
+++ src/app/createchannel/createchannel.component.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+// import customer component
+import { UserlistitemComponent } from '../shared/components/userlistitem/userlistitem.component';
+
+// import classes
+import { Channel } from '../shared/models/channel.class';
+
+// import interfaces
+import { User } from '../shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-createchannel',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,UserlistitemComponent],
   templateUrl: './createchannel.component.html',
   styleUrl: './createchannel.component.scss'
 })
 export class CreatechannelComponent {
+  channel: Channel = new Channel();
+
+  formState: 'channelName' | 'addMember' = 'addMember';
+  memberSelection: 'all' | 'select' = 'all';
+
+  users: User[] = [
+    {
+      name: 'User0',
+      avatarImgPath: 'assets/img/avatar/avatar0.svg',
+      email: 'user0@DABubble.com',
+      status: 'Aktiv',
+      password: ''
+    }, 
+    {
+      name: 'User1',
+      avatarImgPath: 'assets/img/avatar/avatar2.svg',
+      email: 'user1@DABubble.com',
+      status: 'Abwesend',
+      password: ''
+    },
+    {
+      name: 'User2',
+      avatarImgPath: 'assets/img/avatar/avatar3.svg',
+      email: 'user2@DABubble.com',
+      status: 'Aktiv',
+      password: ''
+    },
+    {
+      name: 'User3',
+      avatarImgPath: 'assets/img/avatar/avatar4.svg',
+      email: 'user3@DABubble.com',
+      status: 'Abwesend',
+      password: ''
+    },
+    {
+      name: 'User4',
+      avatarImgPath: 'assets/img/avatar/avatar5.svg',
+      email: 'user4@DABubble.com',
+      status: 'Aktiv',
+      password: ''
+    }
+  ];
+
+  onSubmitName(form:NgForm){
+    if (form.valid) {
+      this.formState = 'addMember';
+    }
+  }
+
+  onSubmit(form:NgForm){
+
+  }
 
 }
---
+++ src/app/shared/components/userlistitem/userlistitem.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, inject } from '@angular/core';
+
+// import interfaces
+import { User } from '../../interfaces/interfaces';
+
+// import services
+import { UserService } from '../../../services/user.service';
+
+@Component({
+  selector: 'app-userlistitem',
+  standalone: true,
+  imports: [],
+  templateUrl: './userlistitem.component.html',
+  styleUrl: './userlistitem.component.scss'
+})
+
+export class UserlistitemComponent {
+
+  userService = inject(UserService);
+
+  @Input({ required: true }) user!:User;
+  @Input({ required: true }) checked!:boolean;
+
+  signedInUser!:User;
+  constructor(){
+    this.userService.user$.subscribe(user => this.signedInUser = user);
+  }
+}
