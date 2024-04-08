fce9f60765e048f00cdf2075fed7ca6a09b6f907
ChristianWestrich <christian.westrich@gmx.de>
2024-04-08 15:13:17 +0200

    generalView -header - dropdown works

--- src/app/general-view/general-view.component.ts
+++ src/app/general-view/general-view.component.ts
@@ -5,20 +5,26 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
+import { UserprofileComponent } from '../userprofile/userprofile.component';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-general-view',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, UserprofileComponent],
   templateUrl: './general-view.component.html',
   styleUrl: './general-view.component.scss'
 })
 export class GeneralViewComponent {
 
-  constructor(private userService: UserService){
+  constructor(private userService: UserService, private router: Router){
     this.unsubscribe = this.userService.user$.subscribe(user => this.activeUser = user)
   }
 
+  showMenu: boolean = false
+  showUserProfile: boolean = false
   activeUser!: User;
   search!: string;
   private unsubscribe!: Subscription
@@ -27,5 +33,22 @@ export class GeneralViewComponent {
     this.unsubscribe.unsubscribe()
   }
 
+  openMenu() {
+    this.showMenu = true
+  }
 
+  openProfile() {
+    this.showUserProfile = true
+  }
+
+  logOut() {
+    this.router.navigate(['/'])
+  }
+
+  closeMenu() {
+    
+    this.showMenu = false
+    this.showUserProfile = false
+
+  }
 }
