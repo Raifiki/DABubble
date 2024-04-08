92c34452e9770bd85068bf7caee885cc6390aeed
Raifikie <leonard_weiss@web.dem>
2024-04-08 07:50:58 +0200

    component createchannel: add Css and Submit/back button to second form part

--- src/app/createchannel/createchannel.component.ts
+++ src/app/createchannel/createchannel.component.ts
@@ -20,7 +20,7 @@ import { User } from '../shared/interfaces/interfaces';
 export class CreatechannelComponent {
   channel: Channel = new Channel();
 
-  formState: 'channelName' | 'addMember' = 'addMember';
+  formState: 'channelName' | 'addMember' = 'channelName';
   memberSelection: 'all' | 'select' = 'all';
 
   users: User[] = [
