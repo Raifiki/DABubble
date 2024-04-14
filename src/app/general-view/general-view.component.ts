6188ba209cf5cd50b67b7e3220b532a21f2cab8d
DavidV0 <davidvelickovic@hotmail.com>
2024-04-14 20:42:16 +0200

    new messages static

--- src/app/general-view/general-view.component.ts
+++ src/app/general-view/general-view.component.ts
@@ -13,31 +13,35 @@ import { OverlayComponent } from './overlay/overlay.component';
 // import interfaces
 import { User } from '../shared/interfaces/interfaces';
 import { LeftSideComponent } from './overlay/left-side/left-side.component';
+import { NewMessageComponent } from './overlay/new-message/new-message.component';
 
 @Component({
   selector: 'app-general-view',
   standalone: true,
   imports: [
-    CommonModule, 
-    FormsModule, 
+    CommonModule,
+    FormsModule,
     OverlayComponent,
-  LeftSideComponent],
+    LeftSideComponent,
+    NewMessageComponent,
+  ],
   templateUrl: './general-view.component.html',
-  styleUrl: './general-view.component.scss'
+  styleUrl: './general-view.component.scss',
 })
 export class GeneralViewComponent {
-
-  constructor(private userService: UserService){
-    this.unsubscribe = this.userService.user$.subscribe(user => this.activeUser = user)
+  constructor(private userService: UserService) {
+    this.unsubscribe = this.userService.user$.subscribe(
+      (user) => (this.activeUser = user)
+    );
   }
 
   activeUser!: User;
   search!: string;
-  private unsubscribe!: Subscription
+  private unsubscribe!: Subscription;
 
   overlayCtrlService = inject(OverlaycontrolService);
 
   ngOnDestroy(): void {
-    this.unsubscribe.unsubscribe()
+    this.unsubscribe.unsubscribe();
   }
 }
