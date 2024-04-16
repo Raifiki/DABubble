f9ef5c134810513b35f3779586eea56a0c37d8c5
ChristianWestrich <christian.westrich@gmx.de>
2024-04-16 07:51:45 +0200

    installed firebase,firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-26c87","appId":"1:464602998455:web:69e854dd4600f5eb11cf32","storageBucket":"dabubble-26c87.appspot.com","locationId":"europe-west","apiKey":"AIzaSyAdtnj-8zQYhytRPXDOpVnKB7Pn2znzIhc","authDomain":"dabubble-26c87.firebaseapp.com","messagingSenderId":"464602998455"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
