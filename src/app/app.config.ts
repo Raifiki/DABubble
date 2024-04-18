7f268b6b68955773dc46e93a46d6cbf78d8622da
Raifikie <leonard_weiss@web.dem>
2024-04-18 07:46:00 +0200

    add new folder and file environment and add firebase config data, authentication: add firestore config data from environment variable, app.config.ts: add firestore config from environment variable

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,12 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environment/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-26c87","appId":"1:464602998455:web:69e854dd4600f5eb11cf32","storageBucket":"dabubble-26c87.appspot.com","apiKey":"AIzaSyAdtnj-8zQYhytRPXDOpVnKB7Pn2znzIhc","authDomain":"dabubble-26c87.firebaseapp.com","messagingSenderId":"464602998455"}))),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideFirestore(() => getFirestore())),
     importProvidersFrom(provideStorage(() => getStorage()))
--- src/app/services/firebase-init.service.ts
+++ src/app/services/firebase-init.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +15,9 @@ export class FirebaseInitService {
   }
 
   private initializeApp() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyAdtnj-8zQYhytRPXDOpVnKB7Pn2znzIhc",
-      authDomain: "dabubble-26c87.firebaseapp.com",
-      projectId: "dabubble-26c87",
-      storageBucket: "dabubble-26c87.appspot.com",
-      messagingSenderId: "464602998455",
-      appId: "1:464602998455:web:69e854dd4600f5eb11cf32"
-    };
+    const firebaseConfig = environment.firebase;
+
+    
     this.app = initializeApp(firebaseConfig);
     this.auth = getAuth(this.app);
   }
