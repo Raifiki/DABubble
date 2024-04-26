d92578970a24d9eb11d140084ebaa3ca9f61bea4
DavidV0 <davidvelickovic@hotmail.com>
2024-04-26 20:32:48 +0200

    Nachrichten laden in channels

--- src/app/general-view/messages/channel-message/channel-message.component.ts
+++ src/app/general-view/messages/channel-message/channel-message.component.ts
@@ -2,16 +2,22 @@ import { Component, Input, OnInit, inject } from '@angular/core';
 import { OverlaycontrolService } from '../../../services/overlaycontrol.service';
 import { CommonModule } from '@angular/common';
 import { ChannelService } from '../../../services/channel.service';
+import { UserService } from '../../../services/user.service';
+import { MessageService } from '../../../services/message.service';
+import { Subscription } from 'rxjs';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 // import classes
 import { Channel } from '../../../shared/models/channel.class';
 import { User } from '../../../shared/models/user.class';
-import { UserService } from '../../../services/user.service';
-import { Observable, Subscription } from 'rxjs';
+import { Message } from '../../../shared/models/message.class';
 
 @Component({
   selector: 'app-channel-message',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './channel-message.component.html',
   styleUrl: './channel-message.component.scss',
 })
@@ -19,20 +25,54 @@ export class ChannelMessageComponent implements OnInit {
   overlayCtrlService = inject(OverlaycontrolService);
   channelService = inject(ChannelService);
   userService = inject(UserService);
+  messageService = inject(MessageService);
+
   user!: User;
+  userUnsub: Subscription;
+
+  activeUser: User = new User();
+  unsubscribeActiveUser;
+
   @Input() channel: Channel = {} as Channel;
 
-  userUnsub: Subscription;
+  messages: Message[] = [];
+  unsubMessages: Subscription;
+
+  newMessage: Message = new Message();
 
   constructor() {
     this.userUnsub = this.userService.activeUser$.subscribe((userData) => {
       this.user = userData;
     });
+
+    this.unsubscribeActiveUser = this.userService.activeUser$.subscribe(
+      (user) => {
+        this.activeUser = user;
+      }
+    );
+
+    this.unsubMessages = this.messageService.messages$.subscribe((messages) => {
+      this.messages = messages;
+    });
+  }
+
+  sendNewMessage() {
+    this.newMessage.creator = this.activeUser;
+    this.newMessage.date = new Date();
+    this.messageService.addMessageToCollection(
+      'Channels',
+      this.channel.id,
+      this.newMessage
+    );
+    this.newMessage = new Message();
+    this.messageService.subMessages('Channels', this.channel.id);
   }
 
   ngOnInit(): void {}
 
   ngOnDestroy() {
     this.userUnsub.unsubscribe();
+    this.unsubMessages.unsubscribe();
+    this.unsubscribeActiveUser.unsubscribe();
   }
 }
