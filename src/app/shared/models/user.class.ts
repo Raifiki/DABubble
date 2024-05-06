1b852e5bd53c868f4d52103f4786bd01053d2ae2
Raifikie <leonard_weiss@web.dem>
2024-05-06 15:40:47 +0200

    newMessage component: add new DM Id to users, bugfix: dont create new DM if DM already exist

--- src/app/shared/models/user.class.ts
+++ src/app/shared/models/user.class.ts
@@ -2,7 +2,7 @@ export class User {
     id: string;
     name: string;
     channelIDs: string[];
-    directMessagesIDs?: string[];
+    directMessagesIDs: string[];
     email: string;
     imgPath: string;
     status?: 'Aktiv' | 'Abwesend';
@@ -14,8 +14,8 @@ export class User {
     constructor(obj?: any) {
       this.id = obj? obj.id : '';
       this.name = obj ? obj.name : '';
-      this.channelIDs = obj ? obj.channelIDs : '';
-      this.directMessagesIDs = obj ? obj.directMessagesIDs : '';
+      this.channelIDs = obj ? obj.channelIDs : [];
+      this.directMessagesIDs = obj ? obj.directMessagesIDs : [];
       this.email = obj ? obj.email : '';
       this.imgPath = obj ? obj.imgPath : '';
       this.status = obj ? obj.status : 'Abwesend';
