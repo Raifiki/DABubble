5feee1cf1be7cf73a4e65a58a32dcdbbdc7bcebd
ChristianWestrich <christian.westrich@gmx.de>
2024-05-16 17:25:33 +0200

    update css

--- src/app/register/sendemail/sendemail.component.ts
+++ src/app/register/sendemail/sendemail.component.ts
@@ -23,7 +23,7 @@ export class SendemailComponent {
   toggleOverlay: boolean = true;
 
   private actionCodeSettings = {
-    url: 'https://christian-westrich.developerakademie.net/Angular/DABubble/#/resetPW/',
+    url: 'https://christian-westrich.de/projects/DABubble/#/resetPW/',
     handleCodeInApp: true,
   };
 
